955c9dc8d0656c38687bf497d24a091e2a012b7f
Hobo12 <daguanglu37@hotmail.com>
2017-07-11 18:53:27 +0800

    1

---
+++ Submit/4rd/HelloGithub/HelloGithub/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello");
+            Console.WriteLine("12");
+            Console.Read();
+           
+        }
+    }
+}
