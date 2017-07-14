b62d4f91b1666ee4341e38788ea99776f2133979
DetryKony <397761549@qq.com>
2017-07-15 00:01:30 +0800

    12.2

---
+++ Submit/4rd/DetryKony/12.2/12.2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input a sentence");
+            string sentence = Console.ReadLine();
+            Console.WriteLine(delete(sentence)); 
+            Console.ReadLine();
+        }
+        private static string delete(string x)
+        {
+            string result = "";
+            string[] arr = x.Split(' ');
+            string target = "o";
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].Contains(target))
+                {
+                    arr[i] = arr[i].Remove(0,arr[i].Length);
+                }
+            }
+            for (int i = 0; i <arr.Length; i++)
+            {
+                if (arr[i]!="")
+                {
+                    result = result + arr[i] + " ";
+                }            
+            }
+            result = result.Trim();
+            return result;
+        }
+    }
+}
