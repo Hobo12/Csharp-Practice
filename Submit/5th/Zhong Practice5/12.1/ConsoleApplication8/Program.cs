a4f8c1b6fa1f037308556b1af4f4c6387b929acb
Zhong-Tianyi <3297405090@qq.com>
2017-07-15 14:59:53 +0800

    Add files via upload

---
+++ Submit/5th/Zhong Practice5/12.1/ConsoleApplication8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("输入一串数字：");
+            string str0 = Console.ReadLine();
+            int str1 = int.Parse(str0);
+            string str2 = string.Format("{0:N}", str1);
+            Console.WriteLine(str2);
+            Console.ReadLine();
+        }
+        
+
+    }
+}
---
+++ Submit/5th/Zhong Practice5/12.2/12.2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("输入一段英文：");
+            string str0 = Console.ReadLine();
+            string str = ' ' + str0 + ' ';
+            int n = str.Length;
+
+            char[] c = str.ToCharArray();
+            for (int i = 0; i < n; i++)
+            {
+                if (c[i] == 'o')
+                {
+                    int j1 = i;
+                    while (c[j1] != ' ')
+                    { c[j1] = '0'; j1--; }
+                    
+                    int j2 = i;
+                    while (c[j2] != ' ')
+                    { c[j2] = '0'; j2++; }
+                    
+                }
+
+            }
+
+            for (int p = 0; p < n; p++)
+            {
+                if (c[p] == '0')
+                {
+                    c[p] = ' ';
+                }
+            }
+
+            string str1 = new string(c);
+            str1 = str1.Trim();
+
+            char[] w = str1.ToCharArray();
+            for (int t = 2; t < str1.Length; t++)
+            {
+                if (w[t] != ' ')
+                { w[t - 1] = '1'; }
+            }
+
+            string str2 = new string(w);
+            str2 = str2.Replace(" ", "");
+            str2 = str2.Replace("1", " ");
+            Console.WriteLine(str2);
+            Console.ReadLine();
+        }
+    }
+}
