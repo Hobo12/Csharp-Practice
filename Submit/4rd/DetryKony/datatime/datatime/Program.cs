8e167c48134bb251a43f595a02724c4f9d665362
DetryKony <397761549@qq.com>
2017-07-20 23:09:24 +0800

    7th

---
+++ Submit/4rd/DetryKony/datatime/datatime/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datatime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myBirthday = new DateTime(1996,2,5);
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+            Console.WriteLine(myAge);
+            Console.Read();
+        }
+    }
+}
---
+++ Submit/7th/DetryKony/7th/7th/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7th
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            box cuboid = new box();
+            Console.WriteLine("Input high ,width ,depth");
+            cuboid.high = Convert.ToDouble(Console.ReadLine());
+            cuboid.width = Convert.ToDouble(Console.ReadLine());
+            cuboid.depth = Convert.ToDouble(Console.ReadLine());
+            Console.Write("volume=");
+            Console.Write(box.volume(cuboid));
+            Console.WriteLine();
+            Console.Write("area=");
+            Console.Write(box.area(cuboid));
+            Console.ReadLine();
+        }
+    }
+    class box
+    {
+        public double high { get; set; }
+        public double width { get; set; }
+        public double depth { get; set; }
+        public static double volume(box x)
+        {
+            return x.high * x.width * x.depth;
+        }
+        public static double area(box x)
+        {
+            return 2 * (x.high * x.width + x.width * x.depth + x.high * x.depth);
+        }
+    }
+}
