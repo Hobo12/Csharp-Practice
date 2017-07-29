cf372e8d793c5043531e555c456907a7b610fe4f
Hobo12 <daguanglu37@hotmail.com>
2017-07-29 10:08:06 +0800

    Hobo12's On-Hand Practice

---
+++ Submit/10th/Hobo12/17.1/17.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("https://ice.do/auth/login");
+            reply += "\nSaved by Hobo12";
+            System.IO.File.WriteAllText(@"WriteText.html", reply);
+        }
+    }
+}
---
+++ Submit/10th/Hobo12/17.2/17.2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader("Objective.txt"))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    String line = sr.ReadToEnd();
+                    Console.WriteLine(line);
+                    
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            string path = "Objective.txt";
+            DateTime localDate = DateTime.Now;
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine("\n"+localDate);
+            }
+
+            Console.ReadLine();
+        }
+        
+    }
+}
