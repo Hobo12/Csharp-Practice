e3f3c298183712ad949275a96903cb6cf5e58c23
Zhong-Tianyi <3297405090@qq.com>
2017-07-31 09:42:36 +0800

    Wallace

---
+++ Submit/10th/10th Zhong/17.1/17.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("https://ice.do/auth/login");
+            Console.WriteLine(reply);
+            File.WriteAllText(@"ice.html", reply + "Saved by Zhong Tianyi");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Submit/10th/10th Zhong/17.2/17.2/17.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = DateTime.Now;
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"Csharp-Practice/Pricatice/The 10th On-hand Practice/Objective.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                System.Console.WriteLine(line);
+                counter++;
+            }
+
+            file.Close();
+            System.Console.WriteLine("There were {0} lines.", counter);
+            // Suspend the screen.
+            System.Console.ReadLine();
+        }
+    }
+}
