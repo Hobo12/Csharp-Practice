211d7f2d30c75726029df2bbb11d7897989c0158
DetryKony <397761549@qq.com>
2017-08-04 22:27:36 +0800

    17

---
+++ Submit/10th/DetryKony/17.1/17.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace _17._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+            string a = client.DownloadString("https://ice.do/auth/login");
+            string b = "\n";
+            string c = "Saved by <DetryKony>";
+            string reply = a + b + c;
+            Console.WriteLine(reply);
+            File.WriteAllText(@"C:\Users\39776\Documents\GitHub\Csharp-Practice\Submit\10th\DetryKony\17.1\ice.html",reply);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Submit/10th/DetryKony/17.2/17.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _17._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a,b,reply;
+            a = File.ReadAllText(@"C:\Users\39776\Documents\GitHub\Csharp-Practice\Submit\10th\DetryKony\17.2\Object.txt");
+            Console.WriteLine(a);
+            b = DateTime.Now.ToString();
+            reply = a + b;
+            File.WriteAllText(@"C:\Users\39776\Documents\GitHub\Csharp-Practice\Submit\10th\DetryKony\17.2\Object.txt",reply);
+            Console.ReadLine();
+
+        }
+    }
+}
